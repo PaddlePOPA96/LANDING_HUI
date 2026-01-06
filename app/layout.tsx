bd36d1458b83c061ce227502bb8ee49eb7df4c88
PaddlePOPA96 <dutahad@gmail.com>
2026-01-06 18:38:21 +0700

    so

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,9 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || "https://www.ferihui.my.id"),
-  alternates: {
-    canonical: '/',
-  },
+
 
   title: {
     default: "Ferihui Stream - Valorant & Gaming Content",
